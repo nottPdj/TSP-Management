24c7e374696ae39fc9755967290529ef388eef9e
Pedro <pedrodfjoao@gmail.com>
2024-05-14 16:36:39 +0100

    main functions

--- src/Management.cpp
+++ src/Management.cpp
@@ -1,1 +1,18 @@
 #include "Management.h"
+
+
+double Management::tspBacktracking(Graph *graph) {
+    return 0;
+}
+
+double Management::tspTriangular(Graph *graph) {
+    return 0;
+}
+
+double Management::tspOther(Graph *graph) {
+    return 0;
+}
+
+double Management::tspRealWorld(Graph *graph, int start) {
+    return 0;
+}
